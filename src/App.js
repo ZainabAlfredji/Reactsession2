f2f4becdbb8b1ad0178bbeeae61cc4a4b2d92867
Zainab Alfredji <zozoti2002@gmail.com>
2022-06-27 21:59:47 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  const handleClick = event => {
+    event.preventDefault();
+
+    console.log('handleClick :', message);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        onChange={handleChange}
+        value={message}
+        autoComplete="off"
+      />
+
+      <h2>Message: {message}</h2>
+
+      <button onClick={handleClick}>Click</button>
     </div>
   );
-}
+};
 
 export default App;
